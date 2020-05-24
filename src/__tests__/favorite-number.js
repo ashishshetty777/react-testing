65d6c40983c4a4057cad0eb506b5633ca8ad285a
Ashish Shetty <ashishshetty777@gmail.com>
2020-05-24 19:01:25 +0530

    @testing-library/react added to replace tl/d

--- src/__tests__/favorite-number.js
+++ src/__tests__/favorite-number.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
-import {getQueriesForElement} from '@testing-library/dom'
+import {render} from '@testing-library/react'
 import {FavoriteNumber} from '../favorite-number'
 
+// function render(ui) {
+//   const container = document.createElement('div')
+//   ReactDOM.render(ui, container)
+//   const queries = getQueriesForElement(container)
+//   return {container, ...queries}
+// }
+
 test('renders a number input with label "Favorite Number"', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<FavoriteNumber />, div)
-  const {getByLabelText} = getQueriesForElement(div)
+  const {getByLabelText} = render(<FavoriteNumber />)
   const input = getByLabelText(/favorite number/i)
   expect(input).toHaveAttribute('type', 'number')
 })
