443c6ef8805e5a6d0212cd6c679b67d995969c6b
Ashish Shetty <ashishshetty777@gmail.com>
2020-05-24 19:03:49 +0530

    debug added

--- src/__tests__/favorite-number.js
+++ src/__tests__/favorite-number.js
@@ -10,7 +10,8 @@ import {FavoriteNumber} from '../favorite-number'
 // }
 
 test('renders a number input with label "Favorite Number"', () => {
-  const {getByLabelText} = render(<FavoriteNumber />)
+  const {getByLabelText, debug} = render(<FavoriteNumber />)
   const input = getByLabelText(/favorite number/i)
   expect(input).toHaveAttribute('type', 'number')
+  debug(input)
 })
