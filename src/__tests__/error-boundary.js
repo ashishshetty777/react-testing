29c40c2e6e3d190203714998e9aa7f51416c4876
Ashish Shetty <ashishshetty777@gmail.com>
2020-05-25 01:11:50 +0530

    Basic error boundary testing

---
+++ src/__tests__/error-boundary.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+import {ErrorBoundary} from '../error-boundary'
+import {reportError as mockReportError} from '../api'
+
+jest.mock('../api')
+
+afterEach(() => {
+  jest.clearAllMocks()
+})
+
+function Bomb({shouldThrow}) {
+  if (shouldThrow) {
+    throw new Error('🏹')
+  } else {
+    return null
+  }
+}
+
+test('testing error boundary component', () => {
+  mockReportError.mockResolvedValueOnce({success: true})
+  const {rerender} = render(
+    <ErrorBoundary>
+      <Bomb />
+    </ErrorBoundary>,
+  )
+
+  rerender(
+    <ErrorBoundary>
+      <Bomb shouldThrow={true} />
+    </ErrorBoundary>,
+  )
+  const error = expect.any(Error)
+  const info = {componentStack: expect.stringContaining('Bomb')}
+  expect(mockReportError).toHaveBeenCalledWith(error, info)
+  expect(mockReportError).toHaveBeenCalledTimes(1)
+})
